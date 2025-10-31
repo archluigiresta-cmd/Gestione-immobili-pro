18296b78b199a7ce6ba8e1f2b85465df647f90e1
archluigiresta-cmd <arch.luigiresta@gmail.com>
2025-10-31 09:10:42 +0100

    chore: Force app version update
    Adds a comment to trigger a version control update for the main application file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// Force update for version control
 import React, { useState, useEffect } from 'react';
 import { User, Project, Screen, ProjectMemberRole, UserStatus, navigationItems, secondaryNavigationItems } from './types';
 import * as dataService from './services/dataService';
